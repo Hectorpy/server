170b5c5fcee349187cc9eedc011fe82a37bb333d
Heitor Mota <128197648+Hectorpy@users.noreply.github.com>
2024-01-31 20:01:09 -0300

    enviando projeto

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cpfCheck = require('cpf-check');
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());  
 app.use(bodyParser.json());
@@ -27,5 +27,5 @@ app.post('/validar-cpf', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando na porta ${port}`);
 });
